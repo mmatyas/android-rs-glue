3efce8e331b6bc6d4d7ba43a125946be64a028b4
Mátyás Mustoha <mmatyas@inf.u-szeged.hu>
2016-11-15 10:57:29 +0100

    Always load `libmain.so`

--- cargo-apk/src/build.rs
+++ cargo-apk/src/build.rs
@@ -363,26 +363,8 @@ import android.util.Log;
 public class MainActivity extends android.app.NativeActivity {{
 
     static {{
-
-        String[] supported_abis;
-
-        try {{
-            supported_abis = (String[]) Build.class.getField("SUPPORTED_ABIS").get(null);
-        }} catch (Exception e) {{
-            // Assume that this is an older phone; use backwards-compatible targets.
-            supported_abis = new String[]{{Build.CPU_ABI, Build.CPU_ABI2}};
-        }}
-
-        boolean matched_an_abi = false;
-
-        for (String abi : supported_abis) {{
-{libs}
-        }}
-
-        if (!matched_an_abi) {{
-            throw new UnsupportedOperationException("Could not find a native abi target to load");
-        }}
-
+        {libs}
+        System.loadLibrary("main");
     }}
 }}"#, libs = libs_string, package_name = config.project_name.replace("-", "_")).unwrap();
 }
